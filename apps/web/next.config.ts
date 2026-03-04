eec68da52aafc13a878714ebd18eca2d3021e1aa
Hopsken <shaoweime@gmail.com>
2026-03-04 15:22:42 +0800

    chore: optimize imports

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,5 +1,14 @@
-import type { NextConfig } from 'next'
+import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {}
+const nextConfig: NextConfig = {
+  experimental: {
+    optimizePackageImports: [
+      "@mantine/core",
+      "@mantine/hooks",
+      "@tanstack/react-query",
+      "lucide-react",
+    ],
+  },
+};
 
-export default nextConfig
+export default nextConfig;
